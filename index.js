1074fcfade3c4ed38b99787a0b3ef519e500263e
SimasDei <simondeiman@gmail.com>
2019-09-27 20:41:01 +0300

    test FS module async callback funcions

--- index.js
+++ index.js
@@ -15,4 +15,15 @@ console.log(textOutput);
 /**
  * @way - Non-Blocking / Asynchronous
  */
-fs.readFile('./txt/start.txt');
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+        console.log('Files have been writter');
+      });
+    });
+  });
+});
