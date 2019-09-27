869503100b5f9bb3c795fef2ab83fe7e4fbeab0b
SimasDei <simondeiman@gmail.com>
2019-09-27 20:30:26 +0300

    try file system synchronous calls

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const hello = 'Ahoy Sailor o/';
 /**
  * @module - Node File System
  */
 const fs = require('fs');
 
+/**
+ * @way - Blocking / Synchronous
+ */
 const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+const textOutput = `Write text to File: ${textInput}.\n Created on: ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOutput);
+console.log(textOutput);
 
-const hello = 'Ahoy Sailor o/';
-console.log(textInput);
+/**
+ * @way - Non-Blocking / Asynchronous
+ */
+fs.readFile('./txt/start.txt');
