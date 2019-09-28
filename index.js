3e3027c5441a6ce07fe8d8193dd5a4a714f27459
SimasDei <simondeiman@gmail.com>
2019-09-28 18:04:36 +0300

    replace template strings in html

--- index.js
+++ index.js
@@ -37,8 +37,32 @@ const url = require('url');
  */
 const http = require('http');
 
+/**
+ * @data - Get product data
+ */
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+/**
+ * @templates - Overview, Card, Product
+ */
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template.overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template.card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template.product.html`, 'utf-8');
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
 
 const server = http.createServer((req, res) => {
   /**
@@ -47,8 +71,17 @@ const server = http.createServer((req, res) => {
 
   const pathName = req.url;
   switch (pathName) {
-    case '/overview' || '/':
-      res.end('This is the Overview');
+    case '/':
+      res.writeHead(200, { 'Content-type': 'text/html' });
+
+      const cardsHtml = dataObj.map(product => replaceTemplate(tempCard, product)).join('');
+      const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+      res.end(output);
+      break;
+    case '/overview':
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(tempOverview);
       break;
     case '/product':
       res.end('This is Product');
